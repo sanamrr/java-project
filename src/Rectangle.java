ea0316f201297384a47e6542f4d93b7e6d3502db
root <root@centos7-master.dlg5l4yerl2eviea0bd04r1meg.ix.internal.cloudapp.net>
2017-10-19 12:27:28 +0000

    initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle{
+     public int length;
+     public int width;
+
+     public Rectangle(int length, int width){
+          this.length=lenth;
+          this.width=width;
+     }
+
+    public int getArea(){
+          return length * width;
+    }
+    public int getPerimeter(){
+          return 2 * (length+width);
+    }
+}
+
+
